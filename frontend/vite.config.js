38f78fb9cd620294ccfaae58aef64de0b3caab6b
Piyush Kumar <piyush31221@gmail.com>
2025-10-23 16:03:19 +0530

    updated package.json file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     allowedHosts: 'all',
     port: 5173,
